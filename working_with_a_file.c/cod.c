d3e33953b4055c0e86113dbc7570b0acfe59bda1
Pavel-Daria <72345301+Pavel-Daria@users.noreply.github.com>
2020-11-28 09:24:04 +0700

    Update cod.c

--- working_with_a_file.c/cod.c
+++ working_with_a_file.c/cod.c
@@ -9,101 +9,99 @@ int mediana(int a[]);
 
 int main(void)
 {
-    int a[size];
-    FILE *f;
-    f = fopen("file.txt", "r");
+  int a[size];
+  FILE *f;
+  f = fopen("file.txt", "r");
 
-    if (f == NULL)
+  if (f == NULL)
+  {
+    printf("Error");
+  }
+
+  else
+  {
+    for (int j = 0; j < size; j++)
     {
-        printf("Error");
+      fscanf(f, "%d", &a[j]);
     }
-
-    else
-    {
-
-        for (int j = 0; j < size; j++)
-        {
-            fscanf(f, "%d", &a[j]);
-        }
-
-        fclose(f);
-    }
-
-
-    f = fopen("file.txt", "a");
-
-    fprintf(f, "\nMaximal number in the array = %d\n", maximum(a));
-    fprintf(f, "Minimal number in the array = %d\n", minimum(a));
-    fprintf(f, "Middle number in the array = %.1f\n", middle(a));
-    fprintf(f, "Median of the array = %d\n", mediana(a));
 
     fclose(f);
+  }
+
+  f = fopen("file.txt", "a");
+
+  fprintf(f, "\nMaximal number in the array = %d\n", maximum(a));
+  fprintf(f, "Minimal number in the array = %d\n", minimum(a));
+  fprintf(f, "Middle number in the array = %.1f\n", middle(a));
+  fprintf(f, "Median of the array = %d\n", mediana(a));
+
+  fclose(f);
 }
 
 int minimum(int a[])
 {
-    int min = a[0];
+  int min = a[0];
 
-    for (int i = 1; i < size; i++)
+  for (int i = 1; i < size; i++)
+  {
+    if (a[i] < min)
     {
-        if (a[i] < min)
-        {
-            min = a[i];
-        }
+      min = a[i];
     }
+  }
 
-    return min;
+  return min;
 }
 
 int maximum(int a[])
 {
-    int max = a[0];
+  int max = a[0];
 
-    for (int i = 1; i < size; i++)
+  for (int i = 1; i < size; i++)
+  {
+    if (a[i] > max)
     {
-        if (a[i] > max)
-        {
-            max = a[i];
-        }
+      max = a[i];
     }
+  }
 
-    return max;
+  return max;
 }
 
 float middle(int a[])
 {
-    double middle = 0;
+  double middle = 0;
 
-    for (int i = 0; i < size; i++)
-    {
-        middle += a[i];
-    }
+  for (int i = 0; i < size; i++)
+  {
+    middle += a[i];
+  }
 
-    return middle / size;
+  return middle / size;
 }
 
 int mediana(int a[])
 {
-    for (int i = 0; i < size - 1; i++)
+  for (int i = 0; i < size - 1; i++)
+  {
+    for (int i = 0; i < size - i - 1; i++)
     {
-        for (int i = 0; i < size - i - 1; i++)
-        {
-            if (a[i] > a[i + 1])
-            {
-                a[i] = a[i] + a[i + 1];
-                a[i + 1] = a[i] - a[i + 1];
-                a[i] = a[i] - a[i + 1];
-            }
-        }
+      if (a[i] > a[i + 1])
+      {
+        a[i] = a[i] + a[i + 1];
+        a[i + 1] = a[i] - a[i + 1];
+        a[i] = a[i] - a[i + 1];
+      }
     }
+  }
 
-    if (size % 2 != 0)
-    {
-        return a[size / 2];
-    }
+  if (size % 2 != 0)
+  {
+    return a[size / 2];
+  }
 
-    else
-    {
-        return (a[size / 2] + a[size / 2 - 1]) / 2;
-    }
+  else
+  {
+    return (a[size / 2] + a[size / 2 - 1]) / 2;
+  }
 }
